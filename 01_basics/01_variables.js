c080c711da00e26c0db87c0e97de4a78771b5709
Ashutoshjaiswal975 <ashutosh.jaiswal.a901@gmail.com>
2024-06-16 20:12:08 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const account_Id =144553
+let account_Email = "ashutosh,jaiswal.a901@gmail.com"
+var account_password = "12345"
+account_city= "jaipur"
+console.log(account_Id);
+/* var is not used in  modern days, because it does not used because it create confusion usig  scope means {}*/
+account_Email = "ashutosh"
+account_password = "12"
+account_city= "punjab"
+let accountState;
+console.table([account_Id, 
+    accountState,account_Email,account_password,account_city])
