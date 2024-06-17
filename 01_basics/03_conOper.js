fabd335d2e0382df3de1e44b345525d19c72e788
Ashutoshjaiswal975 <ashutosh.jaiswal.a901@gmail.com>
2024-06-17 12:40:04 +0000

    operation

---
+++ 01_basics/03_conOper.js
@@ -0,0 +1,13 @@
+// let score ="33"
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// //"33"= >33
+// //"33qbs" => Nan
+// // true => 1; fale=>0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
