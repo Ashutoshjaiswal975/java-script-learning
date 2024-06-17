6ab6b079e9168db08db3828e3967890d65581892
Ashutoshjaiswal975 <ashutosh.jaiswal.a901@gmail.com>
2024-06-17 12:06:16 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hellow") we are using node js not browse
+console.log(3+3);
+console.log("hitesh");
+
+let name="hitesh"
+let age = 18
+let isLoggedIn= false
+
+// number => 2 to power 53
+// string=> ""
+//boolean=> true /false
+//null => stand alone value// it is basically a object
+// undefined => undefined type
+// symbol=> to find the uniqness
+
+// object
+
+
+console.log(typeof "hitesh");
