6f095da0225098a2841bf69076b9a11e4efaf4b6
Ashutoshjaiswal975 <ashutosh.jaiswal.a901@gmail.com>
2024-09-29 19:15:34 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,9 +1,12 @@
-const account_Id =144553
+ const account_Id =144553 // it is paermanent constant... not changable at any cost
 let account_Email = "ashutosh,jaiswal.a901@gmail.com"
 var account_password = "12345"
 account_city= "jaipur"
 console.log(account_Id);
-/* var is not used in  modern days, because it does not used because it create confusion usig  scope means {}*/
+/* var is not used in  modern days, because it does not used because it create confusion usig  scope means {}
+
+because of issue in block scope and fuunction scope
+*/
 account_Email = "ashutosh"
 account_password = "12"
 account_city= "punjab"
