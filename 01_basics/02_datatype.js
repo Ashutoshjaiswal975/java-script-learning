a99b0029bc2088c28b5e5acfd67ccc27e91eef4a
Ashutoshjaiswal975 <ashutosh.jaiswal.a901@gmail.com>
2024-09-29 19:35:40 +0000

    datatypes

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,6 +1,6 @@
 "use strict"; // treat all js code as newer version
 
-// alert("hellow") we are using node js not browse
+// alert("hellow") we are using node js, not browser
 console.log(3+3);
 console.log("hitesh");
 
@@ -9,9 +9,10 @@ let age = 18
 let isLoggedIn= false
 
 // number => 2 to power 53
+//bigint
 // string=> ""
 //boolean=> true /false
-//null => stand alone value// it is basically a object
+//null => it is stand alone  value// it is basically a object-- it is representaion of empty value
 // undefined => undefined type
 // symbol=> to find the uniqness
 
