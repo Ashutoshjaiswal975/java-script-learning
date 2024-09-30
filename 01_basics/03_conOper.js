a72de107a349b4c33e5a10d51e0f3c819a573595
Ashutoshjaiswal975 <ashutosh.jaiswal.a901@gmail.com>
2024-09-30 12:27:20 +0000

    cconversions done

--- 01_basics/03_conOper.js
+++
@@ -1,7 +0,0 @@
-// let score ="33"
-// console.log(typeof score);
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// //"33"= >33
-// //"33qbs" => Nan
-// // true => 1; fale=>0   
---
+++ 01_basics/03_conversion_Operation.js
@@ -0,0 +1,49 @@
+// let score ="33abs"
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+let a="........................"
+// console.log(a);
+// let scor = null
+// console.log(typeof score);
+// let valueInNumbe = Number(scor)
+// console.log(typeof valueInNumbe);
+// console.log(valueInNumbe);
+// //..............................
+// console.log(a);
+// let sco = undefined
+// console.log(typeof sco);
+// let valueInNumb = Number(sco)
+// console.log(typeof valueInNumb);
+// console.log(valueInNumb);
+// //................................
+// console.log(a);
+// let sc = true
+// console.log(typeof sc);
+// let valueInNum = Number(sc)
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+// //................................
+// console.log(a);
+// let isLogedIn= 1
+// let booleanisLogedIn= Boolean(isLogedIn)
+// console.log(booleanisLogedIn);
+// //................................
+// console.log(a);
+// let isLogedIn= ""
+// let booleanisLogedIn= Boolean(isLogedIn)
+// console.log(booleanisLogedIn);
+//................................
+console.log(a);
+let isLogedIn= "hitesh"
+let booleanisLogedIn= Boolean(isLogedIn)
+console.log(booleanisLogedIn);
+
+
+// //"33"= >33
+// //"33qbs" => NaN 
+// // true => 1; fale=>0   
+//1-> true; 0-> false; 
+// ""-> false; 
+// "string"-> true
