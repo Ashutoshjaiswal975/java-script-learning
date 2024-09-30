b0c0ea31e27030ed35ed77b9a4f49e6bc77d38b4
Ashutoshjaiswal975 <ashutosh.jaiswal.a901@gmail.com>
2024-09-30 12:54:51 +0000

    operations done

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,23 @@
+// let stringg= "operations"
+// console.log(stringg);
+// let value= 3
+// let negValue=-value
+// console.log(negValue);
+// console.log(2%3);
+// //.............................
+// let a="..............."
+// console.log(a);
+// console.log("1"+2);
+// console.log(1+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// at what primitive value come first at that pont it will converge
+
+// use of paranthesis
+
+
+//..................................
+console.log(true);
+console.log(+true);
+console.log(+"");
