9c9cc8cdb0d14e4045439e13c4458ba3964e03a6
Ashutoshjaiswal975 <ashutosh.jaiswal.a901@gmail.com>
2024-10-01 18:32:17 +0000

    done

--- 01_basics/06_datatype.js
+++ 01_basics/06_datatype.js
@@ -5,17 +5,17 @@
 
 //  7 types of primitive data type : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -25,16 +25,26 @@ console.log(id === anotherId);
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//...................................
+//stack(primitive):- change value in copy
+//heap(non primitive) memory:-  change by reference means in orignal value
+//...................................
+
+let a= "Ashu" 
+let b= a 
+console.log(a);
+console.log(b);
