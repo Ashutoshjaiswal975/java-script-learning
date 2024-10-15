71f1d849fba5e0342566652f55f3848be90178ed
Ashutoshjaiswal975 <ashutosh.jaiswal.a901@gmail.com>
2024-10-15 17:03:33 +0000

    method how program run

---
+++ control flow /one.js
@@ -0,0 +1,63 @@
+// javascript execution context
+// it run the program in 2 phase
+//  1.global execution context<- refer variable(this)
+// 2. funtion execution context
+// 3. in mongoos another execution context is: Eval execution context
+// in browser execution context: this value :-> window object
+// java script is single threaded
+//....................................
+//javascript code[{ }] <- this will run in 2 phases 1). memory creation phase(creation phase):  only location locate or space, .... 2). execution phase: it ecute the code
+
+
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
